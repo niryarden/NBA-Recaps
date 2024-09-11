8cb332879ecc041257ed20055b8aaae6bda1a6aa
miasegevgal <segevgalmia@gmail.com>
2024-09-11 16:32:09 +0300

    wip

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,10 +1,14 @@
+import json
+
 from overlap_player_names_metric import OverLapPlayerNamesMetric
 from overlap_words_metric import OverLapWordsMetric
 from sbert_similarity_metric import SBERTSimilarityMetric
+from utils import PredictedGame
 
 PLAYER_NAMES_KEY = "player_names"
 SOURCE_TEXT_KEY = "source_text"
 GAME_ID_KEY = "game_id"
+
 
 class NBAMetric:
     def __init__(self):
@@ -12,37 +16,36 @@ class NBAMetric:
         self.overlap_player_names_metric = OverLapPlayerNamesMetric()
         self.sbert_metric = SBERTSimilarityMetric()
 
-    def compute(self, references, predictions):
-        for ref, pred in zip(references, predictions):
-            game_id = ref[GAME_ID_KEY]
-            player_names = ref[PLAYER_NAMES_KEY]
-            ref_val = ref[SOURCE_TEXT_KEY]
+    def compute(self, games):
+        results = {}
+        for game in games:
+            score1 = self.overlap_nba_words_metric.compute_metric(game.reference, game.prediction, game_id=game.game_id)
+            score2 = self.overlap_player_names_metric.compute_metric(game.reference, game.prediction, game.player_names,
+                                                                     game_id=game.game_id)
+            score3 = self.sbert_metric.compute_metric(game.reference, game.prediction, game_id=game.game_id)
+            results[game.game_id] = [score1, score2, score3]
 
-            self.overlap_nba_words_metric.compute_metric(ref_val, pred, game_id=game_id)
-            self.overlap_player_names_metric.compute_metric(ref_val, pred, player_names, game_id=game_id)
-            self.sbert_metric.compute_metric(ref_val, pred, game_id=game_id)
+        return results
 
-        return {
-            "name_overlap_avg_score": self.overlap_player_names_metric.get_average_score(),
-            "nba_overlap_avg_score": self.overlap_nba_words_metric.get_average_score(),
-            "sbert_similarity_avg_score": self.sbert_metric.get_average_score(),
-        }
 
-def load_results_dummy():
-    references = []
-    predictions = []
+def load_results():
+    games = []
+    for i in range(10):
+        with open(f"recaps/test_sample_{i}.json") as f:
+            data = json.load(f)
+            curr_metadata = data["metadata"]
+            game_id = curr_metadata["game_id"]
+            players_names = curr_metadata['home_team_players_names'] + curr_metadata['home_team_names_I'] + \
+                            curr_metadata['away_team_players_names'] + curr_metadata['away_team_names_I']
+            curr_game = PredictedGame(game_id, data["reference_recap"], data["generated_recap"], players_names)
 
-    for i in range(100):
-        curr_ref = open(f"reference/reference_{i}").readlines()
-        references.append(curr_ref)
-        curr_pred = open(f"reference/reference_{i}").readlines()
-        predictions.append(curr_pred)
+            games.append(curr_game)
 
-    return predictions, references
+    return games
 
 
 if __name__ == "__main__":
-    predictions, references = load_results_dummy()
+    games = load_results()
     metric = NBAMetric()
-    scores = metric.compute(references=references, predictions=predictions)
+    scores = metric.compute(games)
     print(scores)
--- utils.py
+++ utils.py
@@ -2,6 +2,14 @@ from datasets import load_dataset
 from config import config
 
 
+class PredictedGame:
+    def __init__(self, game_id, reference, prediction, player_names):
+        self.game_id = game_id
+        self.reference = reference
+        self.prediction = prediction
+        self.player_names = player_names
+
+
 def load_nba_dataset(dataset=config["dataset_name"], limit=None):
     raw_datasets = load_dataset(dataset, trust_remote_code=True)
 
@@ -17,4 +25,3 @@ def load_nba_dataset(dataset=config["dataset_name"], limit=None):
         unsupervised_dataset = unsupervised_dataset.select(range(limit))
 
     return train_dataset, eval_dataset, test_dataset, unsupervised_dataset
-
